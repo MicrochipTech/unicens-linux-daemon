392ab092017f4f8423792ba09567d0a3c4509044
Thorsten Kummermehr <thorsten.kummermehr@k2l.de>
2018-06-13 13:27:55 +0200

    Adding xml2struct tool

---
+++ tool/xml2struct/Xml2Struct.h
@@ -0,0 +1,59 @@
+/*------------------------------------------------------------------------------------------------*/
+/* UNICENS Stucture Printing module                                                               */
+/* Copyright 2018, Microchip Technology Inc. and its subsidiaries.                                */
+/*                                                                                                */
+/* Redistribution and use in source and binary forms, with or without                             */
+/* modification, are permitted provided that the following conditions are met:                    */
+/*                                                                                                */
+/* 1. Redistributions of source code must retain the above copyright notice, this                 */
+/*    list of conditions and the following disclaimer.                                            */
+/*                                                                                                */
+/* 2. Redistributions in binary form must reproduce the above copyright notice,                   */
+/*    this list of conditions and the following disclaimer in the documentation                   */
+/*    and/or other materials provided with the distribution.                                      */
+/*                                                                                                */
+/* 3. Neither the name of the copyright holder nor the names of its                               */
+/*    contributors may be used to endorse or promote products derived from                        */
+/*    this software without specific prior written permission.                                    */
+/*                                                                                                */
+/* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"                    */
+/* AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE                      */
+/* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE                 */
+/* DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE                   */
+/* FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL                     */
+/* DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR                     */
+/* SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER                     */
+/* CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,                  */
+/* OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE                  */
+/* OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.                           */
+/*------------------------------------------------------------------------------------------------*/
+#ifndef UCSXML2STRUCT_H_
+#define UCSXML2STRUCT_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+    
+#include <stdint.h>
+#include "ucs_api.h"
+    
+/*#define C99_STRUCTS*/
+#define TAB "    "
+
+const char *GetXml2StructVersion(void);
+
+void PrintUcsStructures(
+    uint16_t packetBw,
+    Ucs_Rm_Route_t *pRoutes,
+    uint16_t routesSize,
+    Ucs_Rm_Node_t *pNod,
+    uint16_t nodSize,
+    const char *variablePrefix);
+
+void PrintHeaderFile(const char *variablePrefix);
+
+#ifdef __cplusplus
+}
+#endif
+    
+#endif
